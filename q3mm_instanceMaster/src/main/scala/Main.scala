b5847dc4e5f58524d2372353222ca35f32d996b2
Yuriy Netesov <ynetesov@cogniance.com>
2017-02-25 18:24:22 +0100

    ++

--- q3mm_instanceMaster/src/main/scala/Main.scala
+++ q3mm_instanceMaster/src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 import akka.actor._
 
 object Main extends App {
+  Console.println("instance master raising!")
   val system = ActorSystem("q3mm")
   system.actorOf(Props[InstanceMasterActor], name = "instanceMaster")
-  Console.println("hello world!")
 }
